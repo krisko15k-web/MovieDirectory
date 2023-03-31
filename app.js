31ec357ba74e4652655116958222fd4ed356c5a2
krisko15k-web <81244898+krisko15k-web@users.noreply.github.com>
2023-04-01 01:08:39 +0300

    MovieList Update
    Connected to DB and we can add movies with form

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const Movie = require('./models/movie')
 
 const app = express()
 
-mongoose.connect('mongodb://localhost:8080/Movie-Directory')
+mongoose.connect('mongodb+srv://Kris_Parzulov:9909237944kK@moviedirectory.stwojns.mongodb.net/Movie-Directory?retryWrites=true&w=majority')
+    .then((result) => app.listen(8080))
+    .catch((err) => console.log(err))
 
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.render('search', { title: 'Search' })
@@ -19,11 +22,27 @@ app.get('/addMovie', (req, res) => {
 })
 
 app.get('/movieList', (req, res) => {
-    res.render('movieList', { title: 'List' })
+    Movie.find().sort({ createdAt: -1 })
+        .then((result) => {
+            res.render('movieList', { title: 'List', movies: result })
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
+app.post('/movieList', (req, res) => {
+    const movie = new Movie(req.body)
+
+    movie.save()
+        .then((result) => {
+            res.redirect('/movieList')
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About' })
 })
-
-app.listen(8080)
--- models/movie.js
+++ models/movie.js
@@ -4,35 +4,27 @@ const Schema = mongoose.Schema
 const movieSchema = new Schema({
     title: {
         type: String,
-        required: true
     },
     description: {
         type: String,
-        required: true
     },
     director: {
         type: String,
-        required: true
     },
     dateAired: {
         type: Date,
-        required: true
     },
     genre: {
         type: [String],
-        required: true
     },
     duration: {
         type: String,
-        required: true
     },
     score: {
         type: String,
-        required: true
     },
     coverImage: {
         type: String,
-        required: true
     }
 }, { timestamps: true })
 
